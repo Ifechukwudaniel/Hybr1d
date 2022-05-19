fda59d9f0112efcaafbe439dabc0ce9b5b0da079
Angry Coding <mailidanielifechukwu@gmail.com>
2022-05-19 11:55:50 +0100

    completed auth login

--- auth/auth.routes.cofig.ts
+++ auth/auth.routes.cofig.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import {CommonRoutesConfig} from '../common/common.routes.config'
 import authController from './controller/auth.controller';
+import authMiddleware from './middleware/auth.middleware';
 
 
 export class AuthRoutes extends CommonRoutesConfig {
@@ -9,9 +10,15 @@ export class AuthRoutes extends CommonRoutesConfig {
     }
     configureRoutes(): Application {
         this.app.post('/api/auth/register',[
+            authMiddleware.validateBodyRequest,
             authController.registerUser
         ])
-       // this.app.post('/api/auth/login')
+
+       this.app.post('/api/auth/login', [
+           authMiddleware.validateBodyRequest,
+           authMiddleware.validateUserPassword,
+           authController.loginJWT
+       ])
         return this.app
     }
     
--- auth/controller/auth.controller.ts
+++ auth/controller/auth.controller.ts
@@ -2,6 +2,11 @@ import {Request, Response, NextFunction}  from 'express';
 import debug,{IDebugger} from 'debug';
 import argon2 from 'argon2';
 import UsersService from '../../users/services/users.service';
+import jwt from 'jsonwebtoken'
+import crypto from 'crypto';
+
+
+const tokenExpirationInSeconds = 36000;
 
 const log:IDebugger =  debug("app:auth-controller")
 
@@ -11,14 +16,31 @@ class AuthController{
         try {
             req.body.password =  await argon2.hash(req.body.password)
             const userId =  await UsersService.createUser(req.body)
-            res.status(201).send(userId)
+            res.status(201).send({userId})
         } catch (err) {
-            let error =  ""
-            if (typeof err === "string")  error =  err.toUpperCase() 
-            if ( err  instanceof Error)  error =  err.message
-            else error= "Please an error occurred"
-            log("register error: %0",err)
-            res.status(500).send({error})
+            next(err)
+        }
+    }
+    async  loginJWT(req: Request, res: Response, next:NextFunction) {
+      try {
+            let jwtSecret =  process.env.JWT_SECRET || "DATA"
+            const refreshId = req.body.userId +jwtSecret;
+            const salt = crypto.createSecretKey(crypto.randomBytes(16));
+            log(jwtSecret)
+            const hash = crypto
+                .createHmac('sha512', salt)
+                .update(refreshId)
+                .digest('base64');
+            req.body.refreshKey = salt.export();
+            const token = jwt.sign(req.body, jwtSecret, {
+                expiresIn: tokenExpirationInSeconds,
+            });
+            return res
+                .status(201)
+                .send({ accessToken: token, refreshToken: hash });
+        } catch (err) {
+            log('createJWT error: %O', err);
+            return next(err);
         }
     }
 
