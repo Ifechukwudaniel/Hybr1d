389c7ec6bd307118e2a91f9fe0b11f2c49f06503
Angry Coding <mailidanielifechukwu@gmail.com>
2022-05-20 21:18:07 +0100

    tests

--- test/integration/buyers.test.ts
+++ test/integration/buyers.test.ts
@@ -5,43 +5,77 @@ import supertest from 'supertest';
 import mongoose from 'mongoose';
 
 
-let accessToken = '';
+let buyerAccessToken = '';
+let sellerAccessToken= ''
+let seller_id =''
+
 
 describe('Buyers end points', function(){
-    this.slow(300000)
-     let request:supertest.SuperAgentTest
+     let request:supertest.SuperAgentTest = supertest.agent(app)
 
      const buyerUserBody = {
           name:"buyerUser", email: `user+${nanoid(10)}@gmail.com`,password:"password",userType:0
      };
 
+     const products = []
+
      const sellerUserBody = {
         name:"sellerUser", email: `user+${nanoid(10)}@gmail.com`,password:"password", userType:1
     };
 
-     before(function(done){
+    before( async () => {
         request =supertest.agent(app)
-     });
 
-     after(function(){
-         //shutdown express server
-         app.close(()=>{
-             mongoose.connection.close()
-         })
-     })
-    
-    it('should allow a buyers to get list of sellers at /api/buyer/list-of-sellers', async ()=>{
-        const res = await  request.post('/api/auth/register').send(buyerUserBody)
-        expect(res.status).to.equal(201);
-        // returnsetTimeout(done, 300);
+        // login and register buyer user
+        let register_res = await request.post('/api/auth/register').send(buyerUserBody)
+        seller_id =  register_res.body.user_id
+        let res =  await  request.post('/api/auth/login').send(buyerUserBody)
+
+        // login and register seller user
+        await request.post('/api/auth/register').send(sellerUserBody)
+        let sellerRes =  await  request.post('/api/auth/login').send(sellerUserBody)
+
+        //set token as text
+        buyerAccessToken = res.body.accessToken
+        sellerAccessToken = sellerRes.body.accessToken
+    });
+
+
+    it('should allow a buyers get list of sellers at /api/buyer/list-of-sellers', async ()=>{
+       let res =  await  request
+            .get('/api/buyer/list-of-sellers')
+            .set({ Authorization: `Bearer ${buyerAccessToken}` })
+            .send()
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty
+        expect(res.body).to.be.an('object');
+        expect(res.body.sellers).to.be.an('array');
+        expect(res.body.sellers.length).greaterThan(0)
     });
      
     it('should allow buyers get the seller catalog at /api/buyer/seller-catalog/:seller_id', async ()=> {
-        //setTimeout(done, 300);
+        let res =  await  request
+            .get(`/api/buyer/seller-catalog/${seller_id}`)
+            .set({ Authorization: `Bearer ${buyerAccessToken}` })
+            .send()
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty
+        expect(res.body).to.be.an('object');
+        expect(res.body.products).to.be.an('array');
+        for(let product in res.body.products ){
+            products.push(product['_id'])
+        }
     });
 
-    it('should allow buyers create and order to a seller  /api/buyer/create-order/:seller_id', async ()=> {
-       // setTimeout(done, 300);
+    it('should allow buyers create and order /api/buyer/create-order/:seller_id', async ()=> {
+       let res =  await  request
+            .post(`/api/buyer/create-order/${seller_id}`)
+            .set({ Authorization: `Bearer ${buyerAccessToken}` })
+            .send({products})
+        expect(res.status).to.equal(200);
+        expect(res.body).not.to.be.empty
+        expect(res.body).to.be.an('object');
+        // expect(res.body.products).to.be.an('array');
     });
 
 });
--- test/integration/sellers.test.ts
+++ test/integration/sellers.test.ts
@@ -1,46 +1,68 @@
-// import app from '../../app';
-// import {expect} from "chai";
-// import { nanoid } from 'nanoid';
-// import supertest from 'supertest';
-// import mongoose from 'mongoose';
+import app from '../../app';
+import {expect} from "chai";
+import { nanoid } from 'nanoid';
+import supertest from 'supertest';
 
 
-// let accessToken = '';
+let buyerAccessToken = '';
+let sellerAccessToken= ''
+let productId = ''
 
-// describe('Seller end points', () => {
-//      let request:supertest.SuperAgentTest
 
-//      const buyerUserBody = {
-//           name:"buyerUser", email: `user+${nanoid(10)}@gmail.com`,password:"password",userType:0
-//      };
+describe('Seller end points', function(){
+     let request:supertest.SuperAgentTest = supertest.agent(app)
 
-//      const sellerUserBody = {
-//         name:"sellerUser", email: `user+${nanoid(10)}@gmail.com`,password:"password", userType:1
-//     };
+     const buyerUserBody = {
+          name:"buyerUser", email: `user+${nanoid(10)}@gmail.com`,password:"password",userType:0
+     };
+     const product = { name:"Beans Pie" ,price:1000.00 }
 
-//      before(async () => {
-//          request =supertest.agent(app)
-//      });
+     const sellerUserBody = {
+        name:"sellerUser", email: `user+${nanoid(10)}@gmail.com`,password:"password", userType:1
+    };
 
-//      after(()=>{
-//          //shutdown express server
-//          app.close(()=>{
-//              mongoose.connection.close(true)
-//          })
-//      })
-    
-//     it('should allow a seller crete product at /api/buyer/list-of-sellers',  async () => {
-//         await  request.post('/api/auth/register').send(buyerUserBody)
-//         const buyerRes = await  request.post('/api/auth/login').send(buyerUserBody)
-//         accessToken =  buyerRes.body.accessToken    
-//     });
+    before( async () => {
+        request =supertest.agent(app)
+
+        // login and register buyer user
+        await request.post('/api/auth/register').send(buyerUserBody)
+        let res =  await  request.post('/api/auth/login').send(buyerUserBody)
+
+        // login and register seller user
+        await request.post('/api/auth/register').send(sellerUserBody)
+        let sellerRes =  await  request.post('/api/auth/login').send(sellerUserBody)
+
+        //set token as text
+        buyerAccessToken = res.body.accessToken
+        sellerAccessToken = sellerRes.body.accessToken
+    });
+
+
+    it('should allow a sellers to create a product /api/seller/create-product', async ()=>{
+       let res =  await  request
+            .post('/api/seller/create-product')
+            .set({ Authorization: `Bearer ${sellerAccessToken}` })
+            .send(product)
+        expect(res.status).to.equal(201);
+        expect(res.body).not.to.be.empty
+        expect(res.body).to.be.an('object');
+        expect(res.body._id).to.be.a('string');
+        productId = res.body._id
+    });
      
-//     it('should allow buyers get the seller catalog at /api/buyer/seller-catalog/:seller_id',  async () => {
-            
-//     });
+    it('should allow sellers to create catalog /api/seller/create-catalog', async ()=> {
+        let res =  await  request
+            .post('/api/seller/create-catalog')
+            .set({ Authorization: `Bearer ${sellerAccessToken}` })
+            .send(product)
+        expect(res.status).to.equal(201);
+        expect(res.body).not.to.be.empty
+        expect(res.body).to.be.an('object');
+        expect(res.body._id).to.be.a('string');
+    });
 
-//     it('should allow buyers create and order to a seller  /api/buyer/create-order/:seller_id',  async () => {
-             
-//     });
+    it('should allow sellers get orders /api/seller/orders', async ()=> {
+       // setTimeout(done, 300);
+    });
 
-// });
+});
